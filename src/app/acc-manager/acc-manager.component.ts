e57bbf14c246ac5189be74917830dcd3cdafc14f
Malikshi Jayarathna <malikshi.jayarathna@pagero.com>
2023-04-16 22:08:27 +0530

    Changes

--- src/app/acc-manager/acc-manager.component.ts
+++ src/app/acc-manager/acc-manager.component.ts
@@ -46,18 +46,34 @@ export class AccManagerComponent implements OnInit {
     this.items = [
       {
           "itemsId": 1,
-          "name": "1000",
-          "description": "1",
-          "accNo": "12580.80",
-          "price": "Clean",
+          "name": "Portable Tripod",
+          "description": "Promate Precise-150 ",
+          "accNo": "AC0001",
+          "price": "12580.80",
           "status": "true"
         },
         {
-          "itemsId": 1,
-          "name": "1000",
-          "description": "1",
-          "accNo": "12580.80",
-          "price": "Clean",
+          "itemsId": 2,
+          "name": "Camera Travel Backpack",
+          "description": "For Outdoor Photography",
+          "accNo": "AC0002",
+          "price": "12580.80",
+          "status": "true"
+        },
+        {
+          "itemsId": 3,
+          "name": "Chest Mount Harness",
+          "description": "For Action Cameras",
+          "accNo": "AC0003",
+          "price": "17580.80",
+          "status": "true"
+        },
+        {
+          "itemsId": 4,
+          "name": "Laser Wireless Presenter",
+          "description": "Multi-functional Laser Wireless Presenter",
+          "accNo": "AC0004",
+          "price": "12580.80",
           "status": "true"
         }
       ]
