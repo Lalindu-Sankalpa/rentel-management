64e6c2c9fd23dec112422c09ddeb46870c56e22d
Malikshi Jayarathna <malikshi.jayarathna@pagero.com>
2023-04-20 00:18:56 +0530

    changes

--- src/app/cameratype/cameratype.service.ts
+++ src/app/cameratype/cameratype.service.ts
@@ -11,7 +11,7 @@ import { environment } from "src/environments/environment";
 
 export class CameratypeService {
     
-    contactsBaseUrl: string = environment.serverBaseUrl  + '/cameratypes';
+    contactsBaseUrl: string = environment.serverBaseUrl  + '/cameraTypes';
     constructor(private http : HttpClient, private router: Router, public fb: FormBuilder){
     }
 
